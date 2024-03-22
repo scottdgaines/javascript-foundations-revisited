7aad80f7f6180b01bc38f033a484f913f9da5559
Scott Gaines <scottdgaines@gmail.com>
2024-03-22 14:01:53 -0600

    Implement code to pass all but last Dragon test

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,8 +1,35 @@
+class Dragon {
+  constructor(name, rider, temp) {
+    this.name = name,
+    this.rider = rider,
+    this.temperment = temp,
+    this.timesEaten = 0,
+    this.hungry = true
+  }
+}
+
+const createDragon = (name, rider, temp) => {
+  return new Dragon(name, rider, temp)
+}
+
+const greetRider = (dragon) => {
+  return `Hi, ${dragon.rider}!`
+}
+
+const eat = (dragon) => {
+  dragon.timesEaten++
+
+  if (dragon.timesEaten >= 3) {
+    dragon.hungry = false
+  }
+
+  return dragon
+}
 
 
 module.exports = {
-  // createDragon, 
-  // greetRider, 
-  // eat, 
+  createDragon,
+  greetRider,
+  eat
   // findFireBreathers
 }
