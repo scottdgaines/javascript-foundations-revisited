35ed4f4e29d876ce12a0c7823bd4f0603576a6da
Scott Gaines <scottdgaines@gmail.com>
2024-03-23 10:30:14 -0600

    Implement code to pass Hobbit tests 1 - 8

--- mythical-creatures/exercises/hobbit.js
+++ mythical-creatures/exercises/hobbit.js
@@ -1,7 +1,9 @@
 class Hobbit{
   constructor(name, age) {
     this.name = name || 'unknown',
-    this.age = age || 0
+    this.age = age || 0,
+    this.isAdult = false,
+    this.isOld = false
   }
 }
 
@@ -9,10 +11,32 @@ const createHobbit = (name, age) => {
   return new Hobbit(name, age)
 }
 
+const celebrateBirthday = (hobbit) => {
+  hobbit.age++
+
+  if (hobbit.age >= 33) {
+    hobbit.isAdult = true
+  }
+  
+  if (hobbit.age >= 101) {
+    hobbit.isOld = true
+  }
+
+  return hobbit
+}
+
+const getRing = (hobbit) => {
+  if (hobbit.name === "Frodo") {
+    return 'Here is the ring!'
+  } else {
+    return 'You can\'t have it!'
+  }
+}
+
 module.exports = {
-  createHobbit
-  // celebrateBirthday, 
-  // getRing, 
+  createHobbit,
+  celebrateBirthday,
+  getRing,
   // meetPeople, 
   // findFriends
 }
