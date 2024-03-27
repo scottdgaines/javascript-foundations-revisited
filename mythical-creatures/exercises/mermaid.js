2d5ed7b7a24bc58d65c978ddaea988e4742e2784
Scott Gaines <scottdgaines@gmail.com>
2024-03-26 19:17:02 -0600

    Add files for sidekick"

--- mythical-creatures/exercises/mermaid.js
+++ mythical-creatures/exercises/mermaid.js
@@ -1,16 +1,24 @@
+const { createSidekick } = require('/sidekick')
+
 class Mermaid{
-    constructor(name) {
+    constructor(name, friendName) {
         this.name = name,
-        this.fins = true
+        this.fins = true,
+        this.friend = {
+            name: friendName
+        }
     }
 }
 
 const createMermaid = (name) => {
-    return new Mermaid(name)
+    return new Mermaid(name, friendName)
 }
+
+const createFr
 
 module.exports = {
     createMermaid,
+    createSidekick
     // addToCollection,
     // sing
 }
