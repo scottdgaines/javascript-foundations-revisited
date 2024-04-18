7d33872a3b5f39527bbae50ea85105af420d4cba
Scott Gaines <scottdgaines@gmail.com>
2024-04-17 21:35:29 -0600

    Implement code to pass first two tests

--- spotify/spotify.js
+++ spotify/spotify.js
@@ -1,4 +1,25 @@
+class Collection {
+  constructor(name, description) {
+    this.name = name,
+    this.description = description,
+    this.tracks = []
+  }
+}
+class Track {
+  constructor(title, artist, duration) {
+    this.title = title,
+    this.duration = duration,
+    this.artist = artist
+  }
+}
+
+const createCollection = (name, description) => {
+  return collection = new Collection(name, description)
+}
+
+const createTrack = (title, artist, duration) => {
+  return new Track(title, artist, duration)
+}
 
 
-
-  module.exports = {  }
+  module.exports = { createCollection, createTrack }
